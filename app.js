0b094aba6592de505739fbff32506680302d40cc
Ashwath Ashwath <ashwathrox123@gmail.com>
2022-01-05 09:25:34 +0530

    get, post and delete apis written

--- app.js
+++ app.js
@@ -11,6 +11,54 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
 
+mongoose.connect("mongodb://localhost:27017/wikiDB");
+
+const articleSchema = {
+    title: String,
+    content: String,
+};
+
+const Article = mongoose.model("Article", articleSchema);
+
+app.get("/articles", function (req, res) {
+    Article.find(function (err, foundArticles) {
+        // console.log(foundArticles);
+        if (!err) {
+            res.send(foundArticles);
+        } else {
+            res.send(err);
+        }
+    });
+});
+
+app.post("/articles", function (req, res) {
+    // console.log(req.body.title);
+    // console.log(req.body.content);
+
+    const newArticle = new Article({
+        title: req.body.title,
+        content: req.body.content,
+    });
+
+    newArticle.save(function (err) {
+        if (!err) {
+            res.send("Successfully added a new article");
+        } else {
+            res.send(err);
+        }
+    });
+});
+
+app.delete("/articles", function (req, res) {
+    Article.deleteMany(function (err) {
+        if (!err) {
+            res.send("Successfully deleted all articles");
+        } else {
+            res.send(err);
+        }
+    });
+});
+
 app.listen(3000, function () {
     console.log("Server started on port 3000");
 });
